173fb2f988ee26b88601e8656ce79c37a1e78498
BravoWorld <cheoksoonsi@gmail.com>
2015-08-17 13:15:21 +0800

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,13 +1,35 @@
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
 	FILE *fp;
 	char returnData[64];
+	char *returnData2;
+	char *returnData3;
+	int x = 0 , y = 0;
 	fp = popen("/sbin/ifconfig eth0", "r");
 	while (fgets(returnData, 64, fp) != NULL)
 	{
-	 	printf("%s",returnData);
+		returnData2 = strtok(returnData, "\n");
+		while (returnData2 != NULL)
+		{
+			if(x == 1)
+			{
+				returnData3 = strtok(returnData2, " ");
+				while (returnData3 != NULL)
+				{
+					if(y == 1)
+					{
+		 				printf("IP %s\n",returnData3);
+					}
+					returnData3 = strtok(NULL, "  ");
+					y++;
+				}
+			}
+			returnData2 = strtok(NULL, "\n");
+			x++;
+		}
 	}
 	pclose(fp);
 }
